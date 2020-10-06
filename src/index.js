d367df357f32057ea22c695afaae8dc6beb0aa6c
denimcouch <alexmata92@ymail.com>
2020-10-06 15:07:42 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function() {
   // your code here
+  const form = document.querySelector('form')
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    //console.log(e)
+    const newTask = document.getElementById('new-task-description').value;
+
+    createTask(newTask);
+    form.reset()
+  })
 });
+
+function createTask(newTask) {
+  const div = document.createElement('div');
+  const li = document.createElement('li');
+
+  li.innerText = newTask;
+
+  div.append(li)
+
+  const tasks = document.getElementById('tasks')
+  tasks.append(div)
+}
+
